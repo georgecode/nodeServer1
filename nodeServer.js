47d622aca40c9e2bfa64f003571ada9466c137f9
georgecode <georgeczurlanis@gmail.com>
2017-03-20 17:47:20 -0700

    fixing git shit

--- nodeServer.js
+++ nodeServer.js
@@ -1,4 +1,4 @@
-
+var dispatcher = require('httpdispatcher');
 //Lets require/import the HTTP module
 var http = require('http');
 
@@ -6,9 +6,35 @@ var http = require('http');
 const PORT=8080;
 
 //We need a function which handles requests and send response
+// function handleRequest(request, response){
+//     response.end('It Works!! Path Hit: ' + request.url);
+// }
+
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
 }
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+// dispatcher.setStatic('resources');
+
+//A sample GET request
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
 
 //Create a server
 var server = http.createServer(handleRequest);
@@ -16,5 +42,6 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
+    console.log("yoy yoy yo ")
     console.log("Server listening on: http://localhost:%s", PORT);
 });
