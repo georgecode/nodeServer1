c6017421389b76c69d4a1978ea0833939e0c6db3
georgecode <georgeczurlanis@gmail.com>
2017-03-24 02:52:20 -0700

    mongo running node running express running and all working together have a get request to the db working and printing to the browser

--- expressApp/index.js
+++ expressApp/index.js
@@ -1,8 +1,30 @@
+const MongoClient = require('mongodb').MongoClient
 var express = require('express')
 var app = express()
 
+
+//27017
+var db
 app.get('/', function (req, res) {
-  res.send('ZZZFuckXXXHello World!')
+
+  MongoClient.connect('mongodb://localhost:27017/testdb', (err, database) => {
+    db = database
+    db.collection('person').find({"name":"dana"}).toArray()
+    .then(function(shitReturned){
+      console.log(shitReturned[0].name);
+      var nameReturned = shitReturned[0].name;
+
+      res.send('<p>ZZZFuckXXXHello World!</p>'+nameReturned)
+
+      callback(shitReturned);
+
+    })
+    //db.collection('quotes').save
+    // console.log(db.collection('person').find({"name":"dana"}).toArray())
+  // ... do something here
+  })
+
+  // res.send('ZZZFuckXXXHello World!\n'+nameReturned)
 })
 
 app.get('/shit', function (req, res) {
